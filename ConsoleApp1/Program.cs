311a5de0e7ff2a1d0ab57af2c73043a49c689c0f
FelixSuzdalev <felixsuzdalev@yandex.ru>
2024-03-16 00:58:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream FileText = new FileStream("data.txt", FileMode.OpenOrCreate);
+            string lines = "Имя фамилия";
+                using (StreamWriter Writer = new StreamWriter(FileText))
+                {
+                    for (int i = 0; i < 300; i++)
+                    {
+                        {
+                            Writer.WriteLine(lines + $" №{i + 1}");
+                        }
+                    }
+                }
+            string[] data = File.ReadAllLines("data.txt");
+            /*foreach (string line in data)
+            {
+                Console.WriteLine(line);
+            }*/
+        
+        }
+    }
+}
---
+++ _MDK_0104_Practice_12_/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace _MDK_0104_Practice_12_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+            
+        private void Button_Safe(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Имя и фамилия сохранены.\nХотите отобразить все данные из файла?","Cохранение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes) 
+            {
+                Button_Safe1.Background = Brushes.LightCoral;
+            }
+        }
+
+    }
+}
